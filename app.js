31e0ba657c3de76773d033348f3d2edcf5f99a18
jorgegarciaserrano <jogase25@gmail.com>
2024-03-04 17:53:19 +0100

    cambios en estilos, layout introducido, responsiveness

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var favicon = require('serve-favicon');
+var partials = require('express-partials');
 
-//módulos con los routers. Middlewares que se van a utilizar para atender a las rutas. 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup. renderizador de vistas, ejs en este caso.
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,16 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 app.use('/', indexRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler. Esto es un middleware de error. 
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
